6403a33d08ecc447f4ad72d3632a1512fe132116
miell_pelz <folorunsosamueloluwapelumi137@gmail.com>
2022-09-14 07:01:18 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,12 @@
  */
 int print_last_digit(int n)
 {
-	int x;
-	x = (n % 10);
-	if (x < 0)
-	{
-		x = (-1 * x);
-	}
-	_putchar(x + '0');
-	return (x);
+int x;
+x = (n % 10);
+if (x < 0)
+{
+x = (-1 * x);
+}
+_putchar(x + '0');
+return (x);
 }
